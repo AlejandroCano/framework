e9d2bfad006204da4e226f2d3d05e1863c0886f9
Antonio Herranz <antonio.herranz@signumsoftware.com>
2019-01-09 10:52:26 +0100

    Polyfills legacy version: Remove Array.prototype.map

--- Signum.Web/Signum/Scripts/Polyfills.ts
+++ Signum.Web/Signum/Scripts/Polyfills.ts
@@ -92,26 +92,26 @@
     }
 
     if (!Array.prototype.map) {
-        Array.prototype.map = function (fun) {
-            "use strict";
+        //Array.prototype.map = function (fun) {
+        //    //"use strict";
 
-            if (this === void 0 || this === null)
-                throw new TypeError();
+        //    if (this === void 0 || this === null)
+        //        throw new TypeError();
 
-            var t = Object(this);
-            var len = t.length >>> 0;
-            if (typeof fun !== "function")
-                throw new TypeError();
+        //    var t = Object(this);
+        //    var len = t.length >>> 0;
+        //    if (typeof fun !== "function")
+        //        throw new TypeError();
 
-            var res = new Array(len);
-            var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-            for (var i = 0; i < len; i++) {
-                if (i in t)
-                    res[i] = fun.call(thisArg, t[i], i, t);
-            }
+        //    var res = new Array(len);
+        //    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        //    for (var i = 0; i < len; i++) {
+        //        if (i in t)
+        //            res[i] = fun.call(thisArg, t[i], i, t);
+        //    }
 
-            return res;
-        };
+        //    return res;
+        //};
     }
 
     if (!Array.prototype.every)
